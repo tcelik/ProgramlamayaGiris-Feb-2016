e587691539b359483d9a22f2418f20c7b5646671
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 06:43:32 +0300

    Ekrana Merhaba Java yazan programı yazınız

--- src/Sample/src/App.java
+++ src/Sample/src/App.java
@@ -1,9 +1,12 @@
+/*------------------------------------------------------------------------------------------------------------------
+    Ekrana Merhaba Java yazan programı yazınız
+------------------------------------------------------------------------------------------------------------------*/
 import java.io.IOException;
 
 //default packet
 public class App {
     public static void main(String[] args) throws IOException //exception ile uğraşmak istemiyorum.
     {
-        System.out.println("Programlamaya Bilinçli Bir Yaklaşım Kazanma Çabaları, Teşekkürler Ali Hocam.");
+        System.out.println("Merhaba Java");
     }
 }
