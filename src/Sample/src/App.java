03c834a87592afcd8ff68f144a663e77a2758def
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 06:36:23 +0300

    Hello World

---
+++ src/Sample/src/App.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+//default packet
+public class App {
+    public static void main(String[] args) throws IOException //exception ile uğraşmak istemiyorum.
+    {
+        System.out.println("Programlamaya Bilinçli Bir Yaklaşım Kazanma Çabaları, Teşekkürler Ali Hocam.");
+    }
+}
